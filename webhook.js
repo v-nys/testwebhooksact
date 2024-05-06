c1245e34ef2d99290bf01757785d4b6a914ca4b1
Vincent Nys <vincent.nys@ap.be>
2024-05-06 11:47:32 +0200

    Hopelijk genoeg voor de webhook.

---
+++ webhook.js
@@ -0,0 +1,31 @@
+import express from 'express';
+
+const app = express();
+const PORT = 80;
+app.use(express.json());
+// dit is geen Github secret
+// deze code runt normaal op een andere machine dan de workflow
+const VALID_SECRET = 'my-secret-pw';
+
+app.post('/webhook', (req, res) => {
+    const auth_header = req.headers.authorization;
+    if (auth_header && auth_header.startsWith("Bearer ")) {
+        const token = auth_header.substring(7);
+        if (token.trim() == VALID_SECRET) {
+            console.log('Received valid webhook:');
+            res.status(200).send('Webhook processed successfully!');
+        } else {
+            console.log('Gegevens waren ongeldig.');
+            res.status(403).send('Webhook denied.');
+        }
+    }
+    else {
+        console.log('Header ontbrak of formaat klopte niet.');
+        res.status(403).send('Webhook denied.');
+    }
+
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
